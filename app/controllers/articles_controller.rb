fde66e5eaf014eda4c089f27d3ca80b687801105
Gierek1205 <gierek1205@gmail.com>
2016-07-08 13:39:02 +0200

    first commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -48,7 +48,7 @@ end
 
   private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :image_src)
   end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def truncate(s, max=70, elided = ' ...')
+    s.match( /(.{1,#{max}})(?:\s|\z)/ )[1].tap do |res|
+      res << elided unless res.length == s.length
+    end
+  end
+
 end
