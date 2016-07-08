44e265c2122d6d9941e5aca609bf3d82a3fc9adc
Gierek1205 <gierek1205@gmail.com>
2016-07-08 14:43:24 +0200

    koniec kodersow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706083539) do
+ActiveRecord::Schema.define(version: 20160708114244) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
